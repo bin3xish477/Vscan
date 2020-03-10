75e28480ca932727a7fb0b3c66bbd02d0f5a03ba
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-10 13:10:51 -0400

    Update and rename ScanIt.py to scanit.py

---
+++ scripts/scanit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import json
+import request
+
+# 88888888888888888888888888888
+''' 
+purpose: send a GET request
+to VirusTotal to retrieve the 
+scan results.
+param: the md5 hash of the
+file/s that will be scanned
+'''
+# 88888888888888888888888888888
+def get_results(md5_hash):
+  pass
