548b7919087754782d95b7f64146f2215da7e3a4
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-10 13:13:24 -0400

    Update scanit.py

--- scripts/scanit.py
+++ scripts/scanit.py
@@ -3,14 +3,14 @@
 import json
 import request
 
-# 88888888888888888888888888888
+# 88888888888888888888888888888888888888
 ''' 
 purpose: send a GET request
-to VirusTotal to retrieve the 
-scan results.
+         to VirusTotal to retrieve the 
+         scan results.
 param: the md5 hash of the
-file/s that will be scanned
+       file/s that will be scanned
 '''
-# 88888888888888888888888888888
+# 88888888888888888888888888888888888888
 def get_results(md5_hash):
   pass
