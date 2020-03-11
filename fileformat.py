f5abca15b048b03bdcdaa00ffa03a594cd8a83fb
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-11 14:16:23 -0400

    Add files via upload

---
+++ fileformat.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+# **************************** OUTPUT RESULT TO FILE ****************************
+
+import json
+import csv
+
+
+# -------------------------------
+"""
+purpose: output data as CSV
+params: name of file to create,
+data to write to file
+"""
+# -------------------------------
+def toCSV(filename, data):
+
+	try:
+	  with open(filename + '.csv', newline='') as csvfile:
+	    # create csv writer object that will write
+	    # our data in csv format to our file
+	    writeto = csv.writer(csvfile, delimiter=',')
+	    
+	    # the data passed to this function should be
+	    # a list containing lists, where each embedded list
+	    # will create its own row within the csv file
+	    writeto.writerows(data)
+	except:
+		FileError()
+
+
+
+# -------------------------------
+"""
+purpose: output data as JSON
+params: name of file to create,
+data to write to file
+"""
+# -------------------------------
+def toJSON(filename, data):
+
+	try:
+	  with open(filename + '.json') as jsonfile:
+	    # the response from VirusTotal is already in json
+	    # so no need for conversions
+	    json.dump(data, jsonfile)
+	except:
+		FileError()
+
+
+
+# -------------------------------
+"""
+purpose: output data as NORMAL
+params: name of file to create,
+data to write to file
+"""
+# -------------------------------
+def toNORM(filename, data):
+	
+	try:
+	  with open(filename + '.txt') as txtfile:
+	    # the data passed to this function will be
+	    # a dictionary and we'll simply traverse it
+	    # and print key,value pairs seperated by colons
+	    for key, val in data:
+	      txtfile.write(key + ': ' + val, end='\n')
+	except:
+		FileError()
+
+
+
+""" Handling possible file errors """
+def FileError():
+	print('[-] An error occured with a file operation.')
+	exit(0)
---
+++ parser.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+# ****************************** PARSE ARGUMENTS ******************************
+
+from argparse import ArgumentParser
+from colored import fg, attr, bg
+from sys import argv, exit
+from main import programName
+
+
+# -------------------------------
+"""
+purpose: parse arguments passed
+from the command line
+"""
+# -------------------------------
+def ParseArgs():
+
+  parser = ArgumentParser(
+    description=('%s%s* SCAN FILES FOR VIRUSES WITH VSCAN *%s'%(fg(255),bg(1),attr(0))),
+    usage=('vscan %s(-f|--file) [-m|--mass] [--csv]\
+                  \n\t     [--json] [--norm] [-o|--output]%s')%(fg(39),attr(0))
+  )
+  
+  required = parser.add_argument_group('%s%s REQUIRED %s'%(fg(255),bg(2),attr(0)))
+  parser._optionals.title = ('%s%s OPTIONAL %s'%(fg(255),bg(2),attr(0)))
+  
+  # the single file a user wants to scan
+  required.add_argument(
+    '-f', '--file',
+    required=True,
+    dest='single_file',
+    type=str,
+    help='file to scan for virus')
+  
+  # if the user wants to scan more than one file
+  parser.add_argument(
+    '-m','--mass',
+    type=str,
+    dest='mass_file',
+    help='file containing multiple files to scan (50 files max)\
+           \nNote: the timing for multiple scans will depend on the quality of your API key')
+  
+  # if the user wants wants the output format to be pure csv
+  parser.add_argument(
+    '--csv',
+    action='store_true',
+    default=False,
+    help='output scan results to file in csv format')
+
+  # if the user wants the output format to be pure json
+  parser.add_argument(
+    '--json',
+    action='store_true',
+    default=False,
+    help='output scan results to file in json format')
+  
+  # if the user wants the output format to be normal text
+  parser.add_argument(
+    '--norm',
+    action='store_true',
+    default=False,
+    help='output scan results to file with a normal format')
+  
+  # the output file name to create when user chooses
+  # to output scan results to file (json, csv, or normal)
+  parser.add_argument(
+    '-o', '--output',
+    dest='output_file',
+    type=str,
+    help='name of file to create (json, csv, or nornaml)')
+  # if the user wants to provide an API key as an argument
+  parser.add_argument(
+  	'-a', '--apikey',
+  	dest='apikey',
+  	type=str,
+  	default=None,
+  	help='VirusTotal API key to use for scan requests')
+
+  # check if no args were passed
+  # or if too many args were passed
+  if len(argv) == 1 or len(argv) >= 5:
+  	# show program banner
+  	programName()
+  	# print help menu
+  	parser.print_help()
+  	# exit program
+  	exit(1)
+  
+  # get arguments from parser object
+  args = parser.parse_args()
+  
+  return args
