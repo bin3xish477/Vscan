5ad6f5e3a8b6dbb389b887e862a237f48b6009bc
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-12 13:39:45 -0400

    Update interface.py

--- scripts/interface.py
+++ scripts/interface.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+
 # ***************************** PROGRAM INTERFACE ******************************
+
+
 try:
  import main
 except importError:
   print('[-] Could not find the main module')
 
+# --------------------------------------
+"""
+
+"""
+# --------------------------------------
+  
+def interface():
+ 
 
 
