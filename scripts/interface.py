a203cf3b316ea5340aaec8acf59c6dfd4171293e
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-12 13:37:52 -0400

    Create interface.py

---
+++ scripts/interface.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# ***************************** PROGRAM INTERFACE ******************************
+try:
+ import main
+except importError:
+  print('[-] Could not find the main module')
+
+
+
